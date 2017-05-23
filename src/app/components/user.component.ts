d577858e5a9f85e56439a7e162356f8ddcbc2b4a
robyy <roby.yan84@gmail.com>
2017-05-22 21:59:57 -0400

    ng directives, event

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,19 +1,52 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'user',
-  template: `
+    selector: 'user',
+    template: `
   <h1>Hello {{name}}</h1>
   <p><strong>Email:</strong> {{email}}</p>
-  <p><strong>Address:</strong> {{address.street}}  {{address.city}}, {{address.state}}</p>  
+  <p><strong>Address:</strong> {{address.street}}  {{address.city}}, {{address.state}}</p>
+  <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
+  <div *ngIf="showHobbies">
+    <h3>Hobbies</h3>
+    <ul>
+        <li *ngFor='let hobby of hobbies'>
+            {{hobby}}
+        </li>
+    </ul>
+  </div>
   `,
 })
-export class UserComponent  { 
-  name: string = 'Angular'; 
-  email: string = 'john@gmail.com';
-  address = {
-    street: '12 Main st apt 912',
-    city: 'Boston',
-    state: 'MA'
-  };
+export class UserComponent {
+    name: string;
+    email: string;
+    address: address;
+    hobbies: string[];
+    showHobbies: boolean;
+
+    constructor() {
+        this.name = 'Angular';
+        this.email = 'john@gmail.com';
+        this.address = {
+            street: '12 Main st apt 912',
+            city: 'Boston',
+            state: 'MA'
+        };
+        this.hobbies = ['Music', 'Movies', 'Sports'];
+        this.showHobbies = false;
+    }
+
+    toggleHobbies() {
+        if (this.showHobbies === true) {
+            this.showHobbies = false;
+        } else {
+            this.showHobbies = true;
+        }
+    }
+}
+
+interface address {
+    street: string;
+    city: string;
+    state: string;
 }
