afdb9cf3cdb77f22fd7461461b18fb67914671f7
robyy <roby.yan84@gmail.com>
2017-05-27 22:52:05 -0400

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
+// <router-outlet>: now dynamic, depending on whatever route URL we're at
 @Component({
   selector: 'my-app',
   template: `
-    <user></user>
+    <ul>
+      <li><a routerLink="/">Home</a></li>
+      <li><a routerLink="/about">About</a></li>
+    </ul>
+    <hr />
+    <router-outlet></router-outlet>
   `,
 })
-export class AppComponent  { 
-  
+export class AppComponent {
+
 }
